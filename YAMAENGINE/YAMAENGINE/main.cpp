2870689d1a15ad93ded2a9cd8b70039dad5c98e7
kimyongghwa <conna335@gmail.com>
2020-08-12 17:18:32 +0900

    3일차 소전

---
+++ YAMAENGINE/YAMAENGINE/main.cpp
@@ -0,0 +1,21 @@
+
+#include "stdafx.h"
+#include "Console.h"
+#include "Application.h"
+#include "Scene.h"
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR, INT cmdShow) {
+	srand(GetTickCount());
+
+
+	Application app;
+
+	if (!CONSOLE_OFF) {
+		Console console;
+	}
+
+	std::cout << "¾È³ç" << std::endl;
+	app.InitWindow(hInstance);
+	app.InitD3D(app.FloatWindow(hInstance, cmdShow));
+
+	return app.DoMainLoop(new Scene());
+}
